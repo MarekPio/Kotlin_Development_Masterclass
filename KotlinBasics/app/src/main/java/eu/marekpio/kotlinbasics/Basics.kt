c17615cfa67187cd5c0b4efc28374b2154df6858
marekp18@gmail.com <Marek Piotrowski>
2022-02-05 09:25:23 +0100

    [22] Code for Variables and Datatypes

--- KotlinBasics/app/src/main/java/eu/marekpio/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/eu/marekpio/kotlinbasics/Basics.kt
@@ -8,9 +8,14 @@ fun main(){
     // type string
     var myName = "Marek"
     // type int 32 bit
+    // type inference (wnioskowanie o typie) finds out the type from context
     var myAge = 31 // default int (32 bit)
     // write my to display list of my values and their types
 
+    /* Set variable
+       value */
+    myName = "Krzysztof"
+    // const_value_hello = "Error"
 
     // Integer TYPES: Byte (8 bit), Short (16 bit), Int (32 bit), Long (64 bit)
     val myByte: Byte = 125
@@ -22,10 +27,21 @@ fun main(){
     val myFloat: Float = 13.37F // F required for float value
     val myDouble: Double = 3.14159265358979323846 // double type is default
 
-    /* Set variable
-       value */
-    myName = "Krzysztof"
-    // const_value_hello = "Error"
+    // Booleans he type Boolean is used to represent logical values.
+    // It can have two possible values true and false.
+    var myIsSunny: Boolean = true
+    // var isSunny = true // that is the same
+    myIsSunny = false
 
-    print(const_value_hello + myName)
+    // Characters
+    val myLetterChar = 'A'
+    val myDigitChar = '1'
+
+    // Strings
+    val myStr = "Hello World"
+    var firstCharInStr = myStr[0]
+    var lastCharInStr = myStr[myStr.length - 1]
+
+    println("First character " + firstCharInStr)
+    println("Last character " + lastCharInStr)
 }
