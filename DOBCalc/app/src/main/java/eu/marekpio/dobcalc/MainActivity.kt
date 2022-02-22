d222450f98a978d67db956e686fd5a3f4a6bf19e
marekp18@gmail.com <Marek Piotrowski>
2022-02-22 22:28:35 +0100

    [89] Finalizing The Age In Minutes App

--- DOBCalc/app/src/main/java/eu/marekpio/dobcalc/MainActivity.kt
+++ DOBCalc/app/src/main/java/eu/marekpio/dobcalc/MainActivity.kt
@@ -12,6 +12,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
         val btnDataPicker : Button = findViewById(R.id.btnDatePicker)
         tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
 
         btnDataPicker.setOnClickListener {
             clickDatePicker();
@@ -31,7 +33,7 @@ class MainActivity : AppCompatActivity() {
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(this,
+        val dpd = DatePickerDialog(this,
             /* Longer version */
 //          DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
             /* Shorter version with deleted expected function */
@@ -47,11 +49,23 @@ class MainActivity : AppCompatActivity() {
                 val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
                 val theDate = sdf.parse(selectedDate)
 
+                val selectedDateInMinutes = theDate.time / 60000
+
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+                val currentDateInMinutes = currentDate.time / 60000
+
+                val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+
+                tvAgeInMinutes?.text = differenceInMinutes.toString()
+
             },
             year,
             month,
             day
-        ).show()
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
 
 //        Toast.makeText(this,"btnDatePicker pressed", Toast.LENGTH_LONG).show()
     }
