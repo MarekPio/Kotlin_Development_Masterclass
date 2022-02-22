4acb1cd4dcb93740ea27d29b6a200f7712b89397
marekp18@gmail.com <Marek Piotrowski>
2022-02-22 22:16:32 +0100

    [88] Using the state Date to display it at looking into simpledateformat and date

--- DOBCalc/app/src/main/java/eu/marekpio/dobcalc/MainActivity.kt
+++ DOBCalc/app/src/main/java/eu/marekpio/dobcalc/MainActivity.kt
@@ -4,15 +4,21 @@ import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDataPicker : Button = findViewById(R.id.btnDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
 
         btnDataPicker.setOnClickListener {
             clickDatePicker();
@@ -29,8 +35,18 @@ class MainActivity : AppCompatActivity() {
             /* Longer version */
 //          DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
             /* Shorter version with deleted expected function */
-            { view, year, month, dayOfMonth ->
-                Toast.makeText(this,"Datepicker works", Toast.LENGTH_LONG).show()
+            { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this,"Year was $selectedYear" +
+                        ", month was ${selectedMonth+1}" +
+                        ", day was $selectedDayOfMonth", Toast.LENGTH_LONG).show()
+
+                val selectedDate ="$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+                val theDate = sdf.parse(selectedDate)
+
             },
             year,
             month,
